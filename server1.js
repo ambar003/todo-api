94164f7b1b60c7eeed33a070c7ea9bceea5c2b4e
Ambar Gupta <ambargupta7@gmail.com>
2017-01-31 09:37:55 -0800

    Convert DELETE Todo to sequelize

--- server1.js
+++ server1.js
@@ -63,18 +63,21 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted == 1) {
+             res.status(204).send();
+		} else {
+			res.status(404).json({
+				error: 'no todo found'
+			});
+		}
+	}, function() {
+		res.staus(500).send();
 	});
-	if (!matchedTodo) {
-		res.status(404).json({
-			'Error': 'No todo found'
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
-
 });
 
 app.put('/todos/:id', function(req, res) {
