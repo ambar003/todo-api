168ab55f2b850aff9b1649b61a6131510e6bcbe6
Ambar Gupta <ambargupta7@gmail.com>
2017-01-31 15:25:00 -0800

    Add users.js

---
+++ models/user.js
@@ -0,0 +1,70 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
+module.exports = function(sequelize, dataTypes) {
+	var user = sequelize.define('user', {
+		email: {
+			type: dataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		salt: {
+			type: dataTypes.STRING
+		},
+		password_hash: {
+			type: dataTypes.STRING
+		},
+		password: {
+			type: dataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7, 30]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10, value);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string')
+					user.email = user.email.toLowerCase();
+			}
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !(bcrypt.compareSync(body.password, user.get('password_hash')))) {
+							return reject();
+						}
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'email', 'createdAt', 'updatedAt');
+			}
+		}
+	});
+	return user;
+};
